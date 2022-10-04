4bc5a7fef32fc1c7a710954565229e4ef5411846
bridgeminion <sf49ersarthur@gmail.com>
2022-10-03 22:22:36 -0700

    814. Binary Tree Pruning

---
+++ tree/BinaryTreePruning.java
@@ -0,0 +1,15 @@
+package tree;
+
+import common.TreeNode;
+
+public class BinaryTreePruning {
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+
+        return (root.val == 1 || root.left!=null || root.right!=null) ? root : null;
+    }
+}
