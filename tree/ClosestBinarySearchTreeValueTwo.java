eb4bf4ff270b72cfbb00a68ff99ad99215cee973
bridgeminion <sf49ersarthur@gmail.com>
2022-10-05 21:25:57 -0700

    272. Closest Binary Search Tree Value II

--- tree/ClosestBinarySearchTreeValueTwo.java
+++ tree/ClosestBinarySearchTreeValueTwo.java
@@ -6,6 +6,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.PriorityQueue;
 
+/**
+ * Given the root of a binary search tree, a target value, and an integer k, return the k values in the BST that are closest to the target. You may return the answer in any order.
+ *
+ * You are guaranteed to have only one unique set of k values in the BST that are closest to the target.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [4,2,5,1,3], target = 3.714286, k = 2
+ * Output: [4,3]
+ * Example 2:
+ *
+ * Input: root = [1], target = 0.000000, k = 1
+ * Output: [1]
+ */
 public class ClosestBinarySearchTreeValueTwo {
     public List<Integer> closestKValues(TreeNode root, double target, int k) {
         List<Integer> result = new ArrayList<>();
