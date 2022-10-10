7b50ae5578b86bb540e6acfe8b92b970f833b4f8
bridgeminion <sf49ersarthur@gmail.com>
2022-10-09 21:59:35 -0700

    426. Convert Binary Search Tree to Sorted Doubly Linked List

--- common/DoublyNode.java
+++ common/DoublyNode.java
@@ -1,2 +1,20 @@
-package common;public class DoublyNode {
+package common;
+
+public class DoublyNode {
+    public int val;
+    public DoublyNode left;
+    public DoublyNode right;
+
+    public DoublyNode() {
+    }
+
+    public DoublyNode(int _val) {
+        val = _val;
+    }
+
+    public DoublyNode(int _val, DoublyNode _left, DoublyNode _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
 }
