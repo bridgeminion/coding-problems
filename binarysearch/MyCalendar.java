4171b5f7ff3b9257d64d93caa7078f61627a3734
bridgeminion <sf49ersarthur@gmail.com>
2022-10-12 19:53:12 -0700

    729. My Calendar I

--- binarysearch/MyCalendar.java
+++ binarysearch/MyCalendar.java
@@ -1,4 +1,20 @@
 package binarysearch;
 
-public class MyCalendarOne {
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MyCalendar {
+    TreeMap<Integer, Integer> map = new TreeMap<>();
+    public MyCalendar() {
+    }
+
+    public boolean book(int start, int end) {
+        Map.Entry<Integer, Integer> entry = map.lowerEntry(end);
+        if (entry == null || entry.getValue() <= start) {
+            map.put(start, end);
+
+            return true;
+        }
+        return false;
+    }
 }
