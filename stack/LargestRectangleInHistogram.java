204460bb097bceb89819785578564780ec1acfeb
bridgeminion <sf49ersarthur@gmail.com>
2022-10-21 21:43:20 -0700

    84. Largest Rectangle in Histogram

--- stack/LargestRectangleInHistogram.java
+++ stack/LargestRectangleInHistogram.java
@@ -2,9 +2,26 @@ package stack;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Stack;
 
+/**
+ * Given an array of integers heights representing the histogram's bar height where the width of each bar is 1,
+ * return the area of the largest rectangle in the histogram.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: heights = [2,1,5,6,2,3]
+ * Output: 10
+ * Explanation: The above is a histogram where width of each bar is 1.
+ * The largest rectangle is shown in the red area, which has an area = 10 units.
+ * Example 2:
+ *
+ *
+ * Input: heights = [2,4]
+ * Output: 4
+ */
 public class LargestRectangleInHistogram {
     public int largestRectangleArea(int[] heights) {
         Deque<Integer> stack = new ArrayDeque<>();
