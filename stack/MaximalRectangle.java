d4e674afaa7ca6863c57f3a44d50fab61cf072ac
bridgeminion <sf49ersarthur@gmail.com>
2022-10-21 22:02:56 -0700

    85. Maximal Rectangle

--- stack/MaximalRectangle.java
+++ stack/MaximalRectangle.java
@@ -3,6 +3,26 @@ package stack;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+/**
+ * Given a rows x cols binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+ * Output: 6
+ * Explanation: The maximal rectangle is shown in the above picture.
+ * Example 2:
+ *
+ * Input: matrix = [["0"]]
+ * Output: 0
+ * Example 3:
+ *
+ * Input: matrix = [["1"]]
+ * Output: 1
+ */
 public class MaximalRectangle {
     public int maximalRectangle(char[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
